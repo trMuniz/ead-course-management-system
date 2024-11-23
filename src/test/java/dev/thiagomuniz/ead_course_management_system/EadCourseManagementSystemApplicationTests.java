017f2cd71a6b55fa8ca0a929b5d50b0709fd6465
Thiago Muniz <muniz@duck.com>
2024-11-23 13:23:33 -0500

    Initial project structure.

---
+++ src/test/java/dev/thiagomuniz/ead_course_management_system/EadCourseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagomuniz.ead_course_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadCourseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
