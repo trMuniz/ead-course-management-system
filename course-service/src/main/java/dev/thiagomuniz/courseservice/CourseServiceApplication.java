05bcf3bf41b5302867aab8108fc0bc8ea40340ec
Thiago Muniz <muniz@duck.com>
2024-11-25 20:42:54 -0500

    fixed Eureka Server and Eureka Client configuration.

--- course-service/src/main/java/dev/thiagomuniz/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/dev/thiagomuniz/courseservice/CourseServiceApplication.java
@@ -2,7 +2,9 @@ package dev.thiagomuniz.courseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CourseServiceApplication {
     public static void main(String[] args) {
