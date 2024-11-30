c37304e28d65aa3e0666b800d34f52650c05368f
Thiago Muniz <muniz@duck.com>
2024-11-30 01:35:11 -0500

    Updated Course Service cloud configuration.

--- course-service/src/main/java/dev/thiagomuniz/courseservice/CourseServiceApplication.java
+++ course-service/src/main/java/dev/thiagomuniz/courseservice/CourseServiceApplication.java
@@ -3,8 +3,10 @@ package dev.thiagomuniz.courseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 @EnableDiscoveryClient
+@EnableRetry
 @SpringBootApplication
 public class CourseServiceApplication {
     public static void main(String[] args) {
